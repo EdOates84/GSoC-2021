88699d1fbadaf6014ec9efe7f38d846c227a6467
Nitesh <nmahawar@ch.iitr.ac.in>
2021-07-27 20:13:27 +0530

    fix docker

--- get_anchor_info.py
+++ get_anchor_info.py
@@ -19,3 +19,22 @@ while True:
             print(data.json()["search"])
         except:
             print("Invalid Input try again !!!")
+
+# RUN apt-get update -qq -y && \
+#     DEBIAN_FRONTEND=noninteractive apt-get install -y \
+#     	build-essential \
+#         libgtk-3-dev \
+#         libboost-all-dev \
+#         python3 \
+#         python3-pip \
+
+# RUN pip3 install -qq -y && \
+#     DEBIAN_FRONTEND=noninteractive pip3 install -y \
+#     	numpy \
+#         scikit-learn \
+#         pandas \
+#         dlib \
+#         opencv-python \
+#         face_recognition \
+#         matplotlib \
+#         wikipedia \
